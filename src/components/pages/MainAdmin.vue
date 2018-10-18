56302476c436edeeb6ed4b7d2a62a2ab43e50bff
Richard <a247820400@gmail.com>
2018-10-18 18:53:04 +0800

    add sidebar for user & admin

--- src/components/pages/MainAdmin.vue
+++ src/components/pages/MainAdmin.vue
@@ -1,15 +1,54 @@
 <template>
     <div>
-        main-admin
+        <el-container>
+            <el-aside>
+                <el-menu class="m-sidebar"
+                         @open="handleOpen"
+                         @close="handleClose">
+                    <el-menu-item index="1-1">
+                        总览
+                    </el-menu-item>
+
+                    <el-menu-item index="2">
+                        个人信息
+                    </el-menu-item>
+
+                    <el-submenu index="3">
+                        <template slot="title">待审核</template>
+                        <el-menu-item index="3-1">审核出借</el-menu-item>
+                        <el-menu-item index="3-2">审核购买</el-menu-item>
+                        <el-menu-item index="3-3">信用审核</el-menu-item>
+                    </el-submenu>
+
+                    <el-submenu index="4">
+                        <template slot="title">所有记录</template>
+                        <el-menu-item index="4-1">出借记录</el-menu-item>
+                        <el-menu-item index="4-2">购买记录</el-menu-item>
+                        <el-menu-item index="4-3">用户管理</el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+        </el-container>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "main-admin"
+  /* eslint-disable */
+  export default {
+    name: 'main-admin',
+    methods: {
+      handleOpen (key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose (key, keyPath) {
+        console.log(key, keyPath);
+      }
     }
+  };
 </script>
 
 <style lang="scss" scoped>
-
+    .m-sidebar {
+        width: 200px;
+    }
 </style>
--- src/components/pages/MainUser.vue
+++ src/components/pages/MainUser.vue
@@ -1,15 +1,45 @@
 <template>
     <div>
-        main-user
+        <el-container>
+            <el-aside>
+                <el-menu class="m-sidebar"
+                         @open="handleOpen"
+                         @close="handleClose">
+                    <el-menu-item index="1-1">
+                        个人信息
+                    </el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title">个人交易信息</template>
+                        <el-menu-item index="2-1">待审核出借</el-menu-item>
+                        <el-menu-item index="2-2">待审核购买</el-menu-item>
+                        <el-menu-item index="2-3">已完成交易</el-menu-item>
+                    </el-submenu>
+                    <el-menu-item index="3">
+                        信用评级
+                    </el-menu-item>
+                </el-menu>
+            </el-aside>
+        </el-container>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "main-user"
+  /* eslint-disable */
+  export default {
+    name: 'main-user',
+    methods: {
+      handleOpen (key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose (key, keyPath) {
+        console.log(key, keyPath);
+      }
     }
+  };
 </script>
 
 <style lang="scss" scoped>
-
+    .m-sidebar {
+        width: 200px;
+    }
 </style>
